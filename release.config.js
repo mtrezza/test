8f6875ff0c8957bc1db3956da15d42bfff045bfb
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-25 15:59:30 +0200

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -6,6 +6,8 @@ module.exports = {
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
+    ["@semantic-release/npm", {
+      "npmPublish": false,
+    }],
   ],
 };
