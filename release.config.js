d7ba39e74b83f0278e3a404f872ce88d3d1e787c
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-25 16:19:53 +0200

    update

--- release.config.js
+++ release.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  "branches": ["main"],
-  "dryRun": true,
-  "debug": true,
-  "ci": false,
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", {
-      "npmPublish": false,
+  'branches': ['main', 'alpha', 'beta', 'next-major'],
+  'dryRun': true,
+  'debug': true,
+  'ci': false,
+  'plugins': [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', {
+      'npmPublish': false,
     }],
-    ["@semantic-release/changelog", {
-      "changelogFile": "CHANGELOG.md",
+    ['@semantic-release/changelog', {
+      'changelogFile': 'CHANGELOG.md',
     }],
-    ["@semantic-release/git", {
-      "assets": ["CHANGELOG.md"],
+    ['@semantic-release/git', {
+      'assets': ['CHANGELOG.md'],
     }],
   ],
 };
