30c2c29d59feab3442c66ffabe38e4169eb1ee98
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-25 23:33:35 +0200

    feat: 3

--- .releaserc.js
+++ .releaserc.js
@@ -23,12 +23,15 @@ const gitRawCommitsOpts = {
   format: '%B%n-hash-%n%H%n-gitTags-%n%d%n-committerDate-%n%ci%n-authorName-%n%an%n-authorEmail-%n%ae%n-gpgStatus-%n%G?%n-gpgSigner-%n%GS',
 };
 
-// async function readFile(path) {
-//   return await fs.readFile(path, 'utf-8');
-// }
+async function readFile(path) {
+  return await fs.readFile(path, 'utf-8');
+}
 
 // Declare semantic config
 async function config() {
+
+  const template = readFile(changelogTemplateFiles.template);
+  console.log(`config: template: ${template}`);
 
   const config = {
     branches: [
