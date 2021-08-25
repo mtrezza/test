db0ccd141ea132440665f4e414eaf920b6fba2eb
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-25 17:41:14 +0200

    ci(release): add LTS branches
    Adds LTS branches to semnatic-release.

--- release.config.js
+++ release.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  'branches': ['main', 'alpha', 'beta', 'next-major'],
+  'branches': [
+    'main',
+    {name: 'alpha', prerelease: true},
+    {name: 'beta', prerelease: true},
+    'next-major',
+    // Long-Term-Support branches
+    {name: '1.x', range: '1.x', channel: '1.x'},
+    {name: '2.x', range: '2.x', channel: '2.x'},
+    {name: '3.x', range: '3.x', channel: '3.x'},
+    {name: '4.x', range: '4.x', channel: '4.x'},
+    {name: '5.x', range: '5.x', channel: '5.x'},
+    {name: '6.x', range: '6.x', channel: '6.x'},
+  ],
   'dryRun': false,
   'debug': true,
   'ci': false,
