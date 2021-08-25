42b51c701f8b253b8f888a2b25a1fb41c260f50f
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-25 19:29:13 +0200

    ci: update release config

--- release.config.js
+++ release.config.js
@@ -25,15 +25,18 @@ module.exports = {
         { "scope": "no-release", "release": false }
       ]
     }],
-    '@semantic-release/release-notes-generator',
-    ['@semantic-release/npm', {
-      'npmPublish': false,
+    ['@semantic-release/release-notes-generator', {
+
     }],
     ['@semantic-release/changelog', {
       'changelogFile': 'CHANGELOG.md',
     }],
+    ['@semantic-release/npm', {
+      'npmPublish': false,
+    }],
     ['@semantic-release/git', {
-      'assets': ['CHANGELOG.md'],
+      'assets': ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
     }],
   ],
 };
