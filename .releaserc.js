0fc0c8ddd0dbb8a44ebff41dd878a207b4241d72
Manuel <5673677+mtrezza@users.noreply.github.com>
2021-08-25 23:28:35 +0200

    feat: test

--- .releaserc.js
+++ .releaserc.js
@@ -23,62 +23,69 @@ const gitRawCommitsOpts = {
   format: '%B%n-hash-%n%H%n-gitTags-%n%d%n-committerDate-%n%ci%n-authorName-%n%an%n-authorEmail-%n%ae%n-gpgStatus-%n%G?%n-gpgSigner-%n%GS',
 };
 
-// Declare semantic config
-const config = {
-  branches: [
-    'main',
-    { name: 'alpha', prerelease: true },
-    { name: 'beta', prerelease: true },
-    'next-major',
-    // Long-Term-Support branches
-    { name: '1.x.x', range: '1.x.x', channel: '1.x.x' },
-    { name: '2.x.x', range: '2.x.x', channel: '2.x.x' },
-    { name: '3.x.x', range: '3.x.x', channel: '3.x.x' },
-    { name: '4.x.x', range: '4.x.x', channel: '4.x.x' },
-    { name: '5.x.x', range: '5.x.x', channel: '5.x.x' },
-    { name: '6.x.x', range: '6.x.x', channel: '6.x.x' },
-  ],
-  dryRun: false,
-  debug: true,
-  ci: true,
-  tagFormat: '${version}',
-  plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [
-        { type: 'docs', scope: 'README', release: 'patch' },
-        { type: 'refactor', scope: 'core-*', release: 'minor' },
-        { type: 'refactor', release: 'patch' },
-        { scope: 'no-release', release: false },
-      ],
-      parserOpts: {
-        noteKeywords: [ 'BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING' ],
-      },
-    }],
-    ['@semantic-release/release-notes-generator', {
-      preset: 'angular',
-      parserOpts: {
-        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
-      },
-      writerOpts: {
-        commitsSort: ['subject', 'scope'],
-        mainTemplate: await readFile(changelogTemplateFiles.template),
-      },
-    }],
-    ['@semantic-release/changelog', {
-      'changelogFile': changelogFile,
-    }],
-    ['@semantic-release/npm', {
-      'npmPublish': false,
-    }],
-    ['@semantic-release/git', {
-      assets: [changelogFile, 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
-    }],
-  ],
-};
+// async function readFile(path) {
+//   return await fs.readFile(path, 'utf-8');
+// }
 
-async function readFile(path) {
-  return await fs.readFile(path, 'utf-8');
+// Declare semantic config
+async function config() {
+
+  const config = {
+    branches: [
+      'main',
+      { name: 'alpha', prerelease: true },
+      { name: 'beta', prerelease: true },
+      'next-major',
+      // Long-Term-Support branches
+      { name: '1.x.x', range: '1.x.x', channel: '1.x.x' },
+      { name: '2.x.x', range: '2.x.x', channel: '2.x.x' },
+      { name: '3.x.x', range: '3.x.x', channel: '3.x.x' },
+      { name: '4.x.x', range: '4.x.x', channel: '4.x.x' },
+      { name: '5.x.x', range: '5.x.x', channel: '5.x.x' },
+      { name: '6.x.x', range: '6.x.x', channel: '6.x.x' },
+    ],
+    dryRun: false,
+    debug: true,
+    ci: true,
+    tagFormat: '${version}',
+    plugins: [
+      ['@semantic-release/commit-analyzer', {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'refactor', scope: 'core-*', release: 'minor' },
+          { type: 'refactor', release: 'patch' },
+          { scope: 'no-release', release: false },
+        ],
+        parserOpts: {
+          noteKeywords: [ 'BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING' ],
+        },
+      }],
+      ['@semantic-release/release-notes-generator', {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+          // mainTemplate: await readFile(changelogTemplateFiles.template),
+        },
+      }],
+      ['@semantic-release/changelog', {
+        'changelogFile': changelogFile,
+      }],
+      ['@semantic-release/npm', {
+        'npmPublish': false,
+      }],
+      ['@semantic-release/git', {
+        assets: [changelogFile, 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
+      }],
+    ],
+  };
+
+  return config;
 }
 
-module.exports = config;
+
+
+module.exports = await config();
